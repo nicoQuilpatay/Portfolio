5bde50e03c7406dba5676f227824ec7e03458b09
nicoQuilpatay <143046619+nicoQuilpatay@users.noreply.github.com>
2025-05-01 23:00:53 -0300

    Se dejo solamente el boton de demos, ya que todos los repositorios son privados

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -2,7 +2,7 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { CgWebsite } from "react-icons/cg";
-import { BsGithub } from "react-icons/bs";
+//import { BsGithub } from "react-icons/bs";
 
 function ProjectCards(props) {
   return (
@@ -13,6 +13,7 @@ function ProjectCards(props) {
         <Card.Text style={{ textAlign: "justify" }}>
           {props.description}
         </Card.Text>
+         {/* 
         <Button variant="primary" href={props.ghLink} target="_blank">
           <BsGithub /> &nbsp;
           {props.isBlog ? "Blog" : "GitHub"}
@@ -20,6 +21,7 @@ function ProjectCards(props) {
         {"\n"}
         {"\n"}
 
+        */}
         {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
 
         {!props.isBlog && props.demoLink && (
