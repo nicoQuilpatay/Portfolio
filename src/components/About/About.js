7d69d186b3c8677e05b1ccc8f58e6e0de414f132
nicoQuilpatay <143046619+nicoQuilpatay@users.noreply.github.com>
2025-05-01 21:16:46 -0300

    Actualizacion de cv y correccion en about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -34,15 +34,16 @@ function About() {
           </Col>
         </Row>
         <h1 className="project-heading">
-          Professional <strong className="purple">Skillset </strong>
+          Tecnologias y <strong className="purple">Herramientas </strong>
         </h1>
 
         <Techstack />
-
+{/*
         <h1 className="project-heading">
           <strong className="purple">Tools</strong> I use
         </h1>
         <Toolstack />
+          */}
 
         {/* CALENDARIO DE GITHUB
       <Github />
